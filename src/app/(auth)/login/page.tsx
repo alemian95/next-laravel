bf02b4d76dcadb484e652a09fe3e43243125ff54
Alessandro Mian <alessandromian95@gmail.com>
2024-06-26 00:39:23 +0200

    Placeholder login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { errorsType, useAuth } from "@/hooks/useAuth"
+import { useTranslations } from "next-intl"
 import { FormEvent, useState } from "react"
 
 export default function Login() {
@@ -9,6 +10,8 @@ export default function Login() {
         middleware: "guest",
         redirectIfAuthenticated: "/dashboard"
     })
+
+    const t = useTranslations()
 
     const [ errors, setErrors ] = useState<errorsType>({})
     const [ _status, setStatus ] = useState<string | null>(null)
@@ -28,12 +31,19 @@ export default function Login() {
     return (
         <>
             <form onSubmit={submit} className="flex flex-col gap-4 items-center">
+                
                 { errors.email && <span className="text-red-600">{ errors.email }</span> }
-                <input type="email" name="email" id="email" className="border rounded-md p-2 w-full" onChange={event => setEmail(event.target.value)} />
+                <input placeholder={ t('form.login.email') } type="email" name="email" id="email" className="border rounded-md p-2 w-full" onChange={event => setEmail(event.target.value)} />
+                
                 { errors.password && <span className="text-red-600">{ errors.password }</span> }
-                <input type="password" name="password" id="password" className="border rounded-md p-2 w-full" onChange={event => setPassword(event.target.value)} />
-                <input type="checkbox" name="remember" id="remember" className="border rounded-md w-fit" onChange={event => setRemember(event.target.checked)}/>
-                <button type="submit" className="border bg-black text-white uppercase w-fit px-3 py-1 rounded-md" >Login</button>
+                <input placeholder={ t('form.login.password') } type="password" name="password" id="password" className="border rounded-md p-2 w-full" onChange={event => setPassword(event.target.value)} />
+
+                <div className="flex gap-2 items-center">
+                    <label htmlFor="remember">{ t('form.login.remember') }</label>
+                    <input type="checkbox" name="remember" id="remember" className="border rounded-md w-fit" onChange={event => setRemember(event.target.checked)}/>
+                </div>
+                
+                <button type="submit" className="border bg-black text-white uppercase w-fit px-3 py-1 rounded-md" >{ t('form.login.submit') }</button>
             </form>
 
             { pending && "Loading..." }
