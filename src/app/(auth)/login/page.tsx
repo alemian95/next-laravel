55ac9a06b44594a43b97afbc05f2d8cda6360a5b
Alessandro Mian <alessandromian95@gmail.com>
2024-06-28 00:26:53 +0200

    Login form with Zod

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,32 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
 import { errorsType, useAuth } from "@/hooks/useAuth"
 import { useTranslations } from "next-intl"
-import { FormEvent, useState } from "react"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 
 export default function Login() {
+
+    const formSchema = z.object({
+        email: z.string().email(),
+        password: z.string(),
+        remember: z.boolean()
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            remember: false
+        }
+    })
 
     const { login } = useAuth({
         middleware: "guest",
@@ -17,35 +38,54 @@ export default function Login() {
     const [ errors, setErrors ] = useState<errorsType>({})
     const [ _status, setStatus ] = useState<string | null>(null)
     const [ pending, setPending ] = useState<boolean>(false)
-    
-    const [ email, setEmail ] = useState("")
-    const [ password, setPassword ] = useState("")
-    const [ remember, setRemember ] = useState(false)
 
-    async function submit (event : FormEvent<HTMLFormElement>) {
-        event.preventDefault()
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         setPending(true)
-        await login(email, password, remember, setErrors, setStatus)
+        await login(values.email, values.password, values.remember, setErrors, setStatus)
+        if (errors?.email || errors?.password) {
+            values.password = ""
+        }
         setPending(false)
     }
 
     return (
         <>
-            <form onSubmit={submit} className="flex flex-col gap-4 items-center">
-                
-                { errors.email && <span className="text-red-600">{ errors.email }</span> }
-                <input placeholder={ t('form.login.email') } type="email" name="email" id="email" className="border rounded-md p-2 w-full" onChange={event => setEmail(event.target.value)} />
-                
-                { errors.password && <span className="text-red-600">{ errors.password }</span> }
-                <input placeholder={ t('form.login.password') } type="password" name="password" id="password" className="border rounded-md p-2 w-full" onChange={event => setPassword(event.target.value)} />
 
-                <div className="flex gap-2 items-center">
-                    <label htmlFor="remember">{ t('form.login.remember') }</label>
-                    <input type="checkbox" name="remember" id="remember" className="border rounded-md w-fit" onChange={event => setRemember(event.target.checked)}/>
-                </div>
-                
-                <Button animation="3d">{ t('form.login.submit') }</Button>
-            </form>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4 items-center">
+                    
+                    { errors.email && <span className="text-red-600">{ errors.email }</span> }
+                    <FormField control={form.control} name="email" render={({field}) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input placeholder={ t('form.login.email') } {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )} />
+                    
+                    { errors.password && <span className="text-red-600">{ errors.password }</span> }
+                    <FormField control={form.control} name="password" render={({field}) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input type="password" placeholder={ t('form.login.password') } {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )} />
+
+                    <div className="flex gap-2 items-center">
+                        <FormField control={form.control} name="remember" render={({field}) => (
+                            <FormItem className="flex items-center gap-2">
+                                <FormLabel>{ t('form.login.remember') }</FormLabel>
+                                <FormControl>
+                                     <Checkbox name={field.name} value={field.name} onCheckedChange={(checked) => {field.onChange(! field.value)}}/>
+                                </FormControl>
+                            </FormItem>
+                        )} />
+                    </div>
+                    
+                    <Button>{ t('form.login.submit') }</Button>
+                </form>
+            </Form>
 
             { pending && "Loading..." }
         </>
