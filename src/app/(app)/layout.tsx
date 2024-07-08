ded30f64fb749fdb7e025b6d2e56482c56174c97
Alessandro Mian <alessandromian95@gmail.com>
2024-07-08 22:10:19 +0200

    Fix layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useAuth } from "@/hooks/useAuth";
 import { useSettings } from "@/hooks/useSettings";
 import { PropsWithChildren, useState } from "react";
@@ -17,7 +19,7 @@ export default function AppLayout (props : PropsWithChildren) {
     return (
 
         <>
-            <div className="flex flex-col h-screen">
+            <div className="flex flex-col h-screen bg-zinc-50">
                 <header className="p-2 bg-primary text-primary-foreground">
                     <div className="flex justify-between items-center">
                         <h1>layout</h1>
@@ -26,15 +28,16 @@ export default function AppLayout (props : PropsWithChildren) {
                 </header>
                 <div className="flex flex-1">
                     
-                    <aside className={`bg-secondary text-secondary-foreground p-4 w-64 transform lg:translate-x-0 transition-transform duration-300 ease-in-out ${
+                    <aside className={`bg-secondary text-secondary-foreground p-4 w-full lg:w-64 transform lg:translate-x-0 transition-transform duration-300 ease-in-out ${
                         sidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'
-                    } fixed lg:static lg:h-auto h-full flex flex-col items-start justify-between`}>
-                        <div>
-                            sidebar
+                    } fixed top-0 lg:static h-full flex flex-col items-start justify-between`}>
+                        <div className="w-full flex items-center justify-between">
+                            <h1>sidebar</h1>
+                            <button className="block lg:hidden" onClick={() => setSidebarOpen(!sidebarOpen)}>{sidebarOpen ? 'Close Sidebar' : 'Open Sidebar'}</button>
                         </div>
                         <div>
                             <div>
-                                <button onClick={logout}>LOGOUT</button>
+                                <Button variant="destructive" onClick={logout}>LOGOUT</Button>
                             </div>
                             <div>
                                 <small>&copy; { new Date().getFullYear() } - NextJS</small>
@@ -42,10 +45,15 @@ export default function AppLayout (props : PropsWithChildren) {
                         </div>
                     </aside>
 
-                    <main className={`flex-1 bg-zinc-50 p-4 transition-all duration-300 ease-in-out ${sidebarOpen ? 'lg:ml-64' : 'ml-0'}`}>
-                        <pre>{ JSON.stringify(user, null, 2) }</pre>
-                        <pre>{ JSON.stringify(settings, null, 2) }</pre>
-                        { props.children }
+                    <main className={`flex-1 p-4 transition-all duration-300 ease-in-out `}>
+                        <Card>
+                            <CardHeader>
+                                <h1>Dashboard</h1>
+                            </CardHeader>
+                            <CardContent>
+                                { props.children }
+                            </CardContent>
+                        </Card>
                     </main>
                 </div>
             </div>
